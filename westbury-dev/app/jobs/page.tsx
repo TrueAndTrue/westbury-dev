25da4c9dfbe9307b626bb3f74c34d517377cd354
Austin Westbury <austinwestbury@live.com>
2023-10-01 13:50:04 -0400

    file name change

--- westbury-dev/app/jobs/page.tsx
+++ westbury-dev/app/jobs/page.tsx
@@ -5,9 +5,9 @@ export default function Jobs() {
   return (
     <main>
       <section className="flex flex-col min-h-screen items-center p-24">
-        <Testimonial quote={Lewis.quote} name="Lewis Rodiguez" title="Senior Software Engineer" companyURI="/machattle.png" personURI="/lewis.jpg"/>
-        <Testimonial quote={Lewis.quote} name="Lewis Rodiguez" title="Senior Software Engineer" companyURI="/machattle.png" personURI="/lewis.jpg"/>
-        <Testimonial quote={Lewis.quote} name="Lewis Rodiguez" title="Senior Software Engineer" companyURI="/machattle.png" personURI="/lewis.jpg"/>
+        <Testimonial quote={Lewis.quote} name="Lewis Rodiguez" title="Senior Software Engineer" companyURI="/Machattle.png" personURI="/lewis.jpg"/>
+        <Testimonial quote={Lewis.quote} name="Lewis Rodiguez" title="Senior Software Engineer" companyURI="/Machattle.png" personURI="/lewis.jpg"/>
+        <Testimonial quote={Lewis.quote} name="Lewis Rodiguez" title="Senior Software Engineer" companyURI="/Machattle.png" personURI="/lewis.jpg"/>
       </section>
     </main>
   )
